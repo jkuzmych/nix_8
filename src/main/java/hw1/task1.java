acca83b4c75cb4387c3913ac59c6a657d066e3fb
Yuliia Kuzmych <liasvetlaa@gmail.com>
2021-10-11 21:53:43 +0300

    initial commit

---
+++ src/main/java/hw1/task1.java
@@ -0,0 +1,26 @@
+package hw1;
+
+import java.util.Scanner;
+public class task1 {
+
+    public void run()
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input (alphanumerical string): ");
+        String st= in.nextLine();
+
+        String buff="0";
+        int sum = 0;
+        for (int i = 0; i < st.length(); i++) {
+            char ch = st.charAt(i);
+            if (Character.isDigit(ch))
+                buff += ch;
+            else {
+                sum += Integer.parseInt(buff);
+                buff = "0";
+            }
+        }
+        sum += Integer.parseInt(buff);
+        System.out.println("sum of numbers: "+sum);
+    }
+}
